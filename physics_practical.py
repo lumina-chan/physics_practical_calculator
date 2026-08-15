7a29d2665c063cbb74e77c1ae7d4ee4969930e9f
𝕷𝖚𝖒𝖎𝖓𝖆! <adyasha0906@gmail.com>
2026-08-16 01:50:09 +0530

    Enhance vernier_callipers with zero error and corrections
    Added zero error handling and corrected diameter calculations in vernier_callipers function. Updated observation table to include corrected diameter.

--- physics_practical.py
+++ physics_practical.py
@@ -7,7 +7,27 @@ def vernier_callipers():
     #default zero error is zero
     info()
     values = []
+    zero_error = 0
     least_count = 0.01 #cm
+    while True:
+        try:
+            error = ask("Zero error? (+/-/0) : ")
+        except TypeError:
+            pass
+        else:
+            if error == "+" or error == "-" or error == "0":
+                try:
+                    vsd = int(ask("Zero Error VSD: "))
+                    if vsd < 0:
+                        print("VSD is strictly a positive integer or zero.\n")
+                        continue
+                except ValueError:
+                    print("VSD is strictly a positive integer or zero..\n")
+                else:
+                    zero_error = vsd * least_count
+                    break
+            else:
+                print("Choose Type of error: positive, negative, zero.\n")
     while True:
         try:
             n = int(ask("No. of observations you will take? "))
@@ -27,16 +47,23 @@ def vernier_callipers():
                         print("MSR can be positive value with digits after decimal points.\nVSD is strictly a positive integer or zero.\n")
                         continue
                 except ValueError:
-                    print("MSR can be positive value with digits after decimal points.\nVSD is strictly a positive integer.\n")
+                    print("MSR can be positive value with digits after decimal points.\nVSD is strictly a positive integer or zero..\n")
                 else:
                     break
             vsr = vsd * least_count
             ob_ext_diameter = round((msr + vsr), 4) #corrected diameter = observed diameter
+            if error == "+":
+                cr_ext_diameter = ob_ext_diameter - zero_error
+            elif error == "-":
+                cr_ext_diameter = ob_ext_diameter + zero_error
+            else:
+                cr_ext_diameter = ob_ext_diameter
             ob = {"observation_no": (_ + 1),
                 "msr": msr,
                 "vsd": vsd,
                 "vsr": vsr,
-                "observed_diameter": ob_ext_diameter}
+                "observed_diameter": ob_ext_diameter,
+                "corrected_diameter":cr_ext_diameter}
             values.append(ob)
         observation_table(values)
         diameter = avg(values)
@@ -57,16 +84,16 @@ def ask(prompt):
 def avg(values):
     mean = 0
     for ob in values:
-        mean += ob["observed_diameter"]
+        mean += ob["corrected_diameter"]
     return mean/len(values)
 
 def observation_table(values):
     print("\nOBSERVATION TABLE")
     print("-" * 60)
-    print(f"{"Obs no.":<12}{"MSR":<12}{"VSD":<12}{"VSR":<12}{"Obs diameter":<12}")
+    print(f"{"Obs no.":<12}{"MSR":<12}{"VSD":<12}{"VSR":<12}{"Obs diameter":<12}{"Crr diameter":<12}")
     print("-" * 60)
     for ob in values:
-        print(f"{ob["observation_no"]:<12}{ob["msr"]:<12}{ob["vsd"]:<12}{ob["vsr"]:<12}{ob["observed_diameter"]:<12}")
+        print(f"{ob["observation_no"]:<12}{ob["msr"]:<12}{ob["vsd"]:<12}{ob["vsr"]:<12}{ob["observed_diameter"]:<12}{ob["corrected_diameter"]:<12}")
     print("-" * 60)
 
 main()
